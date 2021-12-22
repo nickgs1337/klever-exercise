d9acd857bcacee77550da93f834e1a702d5d9472
nickgs1337 <nicklatim@live.com>
2021-12-22 08:43:27 -0300

    Create basic cryptocurrency file structure

---
+++ cryptocurrency/cryptocurrency.go
@@ -0,0 +1,76 @@
+package cryptocurrency
+
+import "golang.org/x/net/context"
+
+type Server struct {
+	connections []*Connection
+}
+
+func (server *Server) Create(context context.Context, message *CryptocurrencyMessage) (*CryptocurrencyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) Get(context context.Context, symbol *CryptocurrencySymbol) (*CryptocurrencyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) List(context context.Context, message *EmptyMessage) (*CryptocurrencyListMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) Update(context context.Context, request *UpdateCryptocurrencyRequest) (*CryptocurrencyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) Delete(context context.Context, symbol *CryptocurrencySymbol) (*EmptyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) UpVote(context context.Context, symbol *CryptocurrencySymbol) (*CryptocurrencyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) DownVote(context context.Context, symbol *CryptocurrencySymbol) (*CryptocurrencyMessage, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) CreateVoteStream(symbol *CryptocurrencySymbol, stream CryptocurrencyService_CreateVoteStreamServer) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (server *Server) mustEmbedUnimplementedCryptocurrencyServiceServer() {
+	//TODO implement me
+	panic("implement me")
+}
+
+type Connection struct {
+	stream CryptocurrencyService_CreateVoteStreamServer
+	symbol string
+	err    chan error
+}
+
+func fromMessage(message *CryptocurrencyMessage) *Cryptocurrency {
+	return &Cryptocurrency{
+		Symbol:  message.Symbol,
+		Name:    message.Name,
+		IconURL: message.IconURL,
+		Votes:   0,
+	}
+}
+
+func toMessage(cryptocurrency *Cryptocurrency) *CryptocurrencyMessage {
+	return &CryptocurrencyMessage{
+		Symbol:  cryptocurrency.Symbol,
+		Name:    cryptocurrency.Name,
+		IconURL: cryptocurrency.IconURL,
+		Votes:   cryptocurrency.Votes,
+	}
+}

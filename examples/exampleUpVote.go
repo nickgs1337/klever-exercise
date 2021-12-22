84468e4407f2d4de9ce74e62971df9902f7dd6e6
nickgs1337 <nicklatim@live.com>
2021-12-22 08:52:50 -0300

    Create example to call UpVote method

---
+++ examples/exampleUpVote.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"klever/cryptocurrency"
+	"log"
+)
+
+func main() {
+
+	// setup grpc connection
+	var conn *grpc.ClientConn
+	conn, err := grpc.Dial(":8000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %s", err)
+	}
+	defer conn.Close()
+
+	// use CryptocurrencyClient
+	c := cryptocurrency.NewCryptocurrencyServiceClient(conn)
+
+	// up vote a currency
+	newCurrency := cryptocurrency.CryptocurrencySymbol{
+		Symbol: "BTC",
+	}
+
+	response, err := c.UpVote(context.Background(), &newCurrency)
+	if err != nil {
+		log.Fatalf("Could not connect: %s", err)
+	}
+	log.Printf("Symbol %s\n", response.Symbol)
+	log.Printf("Name %s\n", response.Name)
+	log.Printf("Icon %s\n", response.IconURL)
+	log.Printf("Votes %d\n", response.Votes)
+}

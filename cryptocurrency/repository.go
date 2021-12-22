f4c5d4c456f20668b59236c58de066573fbc37df
nickgs1337 <nicklatim@live.com>
2021-12-22 08:35:00 -0300

    Add GetBySymbol behaviour to repository

--- cryptocurrency/repository.go
+++ cryptocurrency/repository.go
@@ -1,11 +1,25 @@
 package cryptocurrency
 
 import (
+	"context"
+	"errors"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"klever/utils"
 )
 
 const collection = "cryptocurrency"
+
+func GetBySymbol(symbol string) (*Cryptocurrency, error) {
+	var cryptocurrency Cryptocurrency
+	err := getCollection().FindOne(context.TODO(), bson.D{{"_id", symbol}}).Decode(&cryptocurrency)
+
+	if err != nil {
+		return nil, errors.New("currency not found")
+	}
+
+	return &cryptocurrency, err
+}
 
 func getCollection() *mongo.Collection {
 	return utils.GetDatabase().Collection(collection)
